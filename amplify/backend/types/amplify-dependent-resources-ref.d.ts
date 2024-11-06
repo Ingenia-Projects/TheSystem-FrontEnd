6527677856aadc4346cfd8f87833dcd10de7aa66
Miguel <miguel@ingeniaprojects.com>
2024-11-07 00:16:47 +0100

    Agregar autenticación con Cognito usando Authenticator

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "thesystemb8db0840": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
+import React from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';  // Usa Authenticator en lugar de AmplifyAuthenticator
+import '@aws-amplify/ui-react/styles.css';  // Importa estilos de UI de Amplify
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (  // Proporciona acceso a signOut y user
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1>Hola, {user.username}</h1>
+            <p>
+              Edita <code>src/App.js</code> y guarda para recargar.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Aprende React
+            </a>
+            <button onClick={signOut}>Cerrar sesión</button> {/* Botón de Cerrar sesión */}
+          </header>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
